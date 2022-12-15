acb68a94df5749919ab51233bf24d18012b3f470
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2022-12-15 15:58:08 -0500

    Main.js window resolution change
    Nice cozy 480p resolution

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const path = require('path')
 const createWindow = () => {
 
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 852,
+    height: 480,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
