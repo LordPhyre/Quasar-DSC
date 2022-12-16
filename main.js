98de18958cd4d17cbb68d5e0a6cef577f5c2f4c5
jcjms <jesse.zurek@icloud.com>
2022-12-16 13:11:34 +0100

    back to old

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+  const mainWindow = new BrowserWindow({
+    width: 852,
+    height: 480,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-  win.loadFile('index.html');
-};
+  //BrowserWindow.maximize()
+  mainWindow.removeMenu()
+  mainWindow.loadURL('https://deadshot.io')
+}
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  if (process.platform !== 'darwin') app.quit()
+})
