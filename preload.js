c63016bc5920ddfdf15365b038d140d1fb785d38
jcjms <jesse.zurek@icloud.com>
2022-12-16 21:37:00 +0100

    gg button

--- preload.js
+++ preload.js
@@ -5,6 +5,37 @@ document.addEventListener("keydown", (event) => {
   }
 })
 
+let btn = document.createElement("button");
+btn.innerHTML = "GG";
+btn.type = "submit";
+btn.id = "ggbtn";
+btn.style = "position: absolute;left: 100;top: 100;z-index: 1000;";
+document.body.appendChild(btn);
+
+document.getElementById('ggbtn').onclick = function() {
+    //alert("it should work");
+    //document.getElementById('input').value = 'GG';
+
+    const input = document.querySelector('input[placeholder="[Enter] to use chat"]');
+    input.value = 'GG';
+
+    //input.click();
+
+    /*const event = new KeyboardEvent('keydown', {
+        key: 'Enter',
+        bubbles: true,
+        cancelable: true
+    });*/
+
+    const event = new KeyboardEvent('keydown', {
+        keyCode: 13,
+        bubbles: true,
+        cancelable: true
+    });
+      
+    input.dispatchEvent(event);
+};
+
 // Settings Stuff
 document.addEventListener("DOMContentLoaded", (event) => {
   ipcRenderer.send('preloadNeedSettings');
